964458802f4361a3facacf07e8833bc263ce9569
milloescobar <milloescobar@gmail.com>
2015-11-02 14:21:04 -0600

    reel

--- js/main.js
+++ js/main.js
@@ -17,3 +17,12 @@ getId("show-pay-form").onclick = function() {
 		getId("show-pay-form").style.display='none';
 	}, 1000);	
 };
+
+getId("play-gen-video").onclick = function() {
+	var iframe = getId('gen-video');
+	var player = $f(iframe);
+	player.api("play");
+	getId("video-generation-info").style.display='none';
+	console.log('play-video');
+};
+

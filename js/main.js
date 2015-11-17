5121b6d69aa68376a9965929dfa07c3efab63775
milloescobar <milloescobar@gmail.com>
2015-11-17 15:28:18 -0600

    js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,31 @@
-Function.prototype.method = function(name, func){
-  if(!this.prototype[name]){
-      this.prototype[name] = func;
-  } 
-  return this;
-}
+(function (window, undefined) {
+  function getId(c){
+    var elElemento = document.getElementById(c);
+   return elElemento;
+  }
 
-function getId(c){
-  var elElemento = document.getElementById(c);
- return elElemento;
-}
+  getId("show-pay-form").onclick = function() {
+    this.classList.toggle('hide-button');
+    getId("flip-wrap").classList.toggle('hide-flip-wrap');
+    getId("pay-form").classList.toggle('show-pay-form');
+    setTimeout(function(){
+      getId("show-pay-form").style.display='none';
+    }, 2000);
+  };
 
-getId("show-pay-form").onclick = function() {
-	this.classList.toggle('hide-button');
-	getId("flip-wrap").classList.toggle('hide-flip-wrap');
-	getId("pay-form").classList.toggle('show-pay-form');
-	setTimeout(function(){
-		getId("show-pay-form").style.display='none';
-	}, 2000);	
-};
+  getId("play-gen-video").onclick = function() {
+    var iframe = getId('gen-video');
+    var player = $f(iframe);
+    player.api("play");
+    getId("video-generation-info").style.display='none';
+    console.log('play-video');
+  };
 
-getId("play-gen-video").onclick = function() {
-	var iframe = getId('gen-video');
-	var player = $f(iframe);
-	player.api("play");
-	getId("video-generation-info").style.display='none';
-	console.log('play-video');
-};
-
-getId("play-descripcion-video").onclick = function() {
-	var iframe = getId('description-video');
-	var player = $f(iframe);
-	player.api("play");
-	getId("video-descripcion-info").style.display='none';
-	console.log('play-video');
-};
+  getId("play-descripcion-video").onclick = function() {
+    var iframe = getId('description-video');
+    var player = $f(iframe);
+    player.api("play");
+    getId("video-descripcion-info").style.display='none';
+    console.log('play-video');
+  };
+})(window);
